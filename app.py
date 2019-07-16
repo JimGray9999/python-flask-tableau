844e4378f57d4d8ac659afaf30d41bd0d2e03e21
Jim Gray <jimgray2005@gmail.com>
2019-07-16 17:40:42 -0400

    updated tableau and templates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
+app = Flask(__name__, static_folder="templates")
 
 @app.route("/")
 def home():
@@ -8,7 +8,7 @@ def home():
 
 
 @app.route("/tableau")
-def home():
+def tableau():
     return render_template('tableau.html')
 
 if __name__ == '__main__':
